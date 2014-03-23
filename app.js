cb4930cfb3040b1e28af4f9ff72aa20688b542ba
Eniz Gülek <enizgulek@gmail.com>
2014-03-24 00:03:30 +0200

    .gitignore is now working

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
+  , hbs = require('hbs')
   , path = require('path');
 
 var app = express();
@@ -14,7 +14,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -36,8 +36,6 @@ app.get('/about', function (req, res) {
 app.get('/contact', function (req, res) {
   res.render('contact', { title: 'Contact'});
 });
-
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
